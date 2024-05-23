8b0b73835c8437a1964bc35c5db43224f1a898fa
Fabian <fab27fc@gmail.com>
2024-05-22 22:49:00 -0600

    Revert "Added week number 5"
    This reverts commit 52a52ab864eaa6b8a4ed76a7ea054c77feaf929a.

--- semana-5/ejercicio2_diccionarios.py
+++
@@ -1,9 +0,0 @@
-list_a = ['first_name', 'last_name', 'role']
-list_b = ['Fabian', 'Fernandez', 'Software Engineer']
-
-diccionario = {}
-
-for i in range(len(list_a)):
-    diccionario[list_a[i]] = list_b[i]
-
-print(diccionario)
--- semana-5/ejercicio4_elimina_impares.py
+++
@@ -1,8 +0,0 @@
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-print(my_list)
-
-for numero in my_list[:]:
-    if numero % 2 != 0:
-        my_list.remove(numero) 
-
-print(my_list)
